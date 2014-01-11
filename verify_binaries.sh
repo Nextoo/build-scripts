ed82bdc4fe25cecd367aff11324d86ffcff2a755
Benjamin Mann <bjmn55@gmail.com>
2014-01-11 02:30:54 -0800

    Submitting the correct file

--- verify_binaries.sh
+++ verify_binaries.sh
@@ -1,34 +1,119 @@
 #!/bin/bash
-# Test my 'utils.sh'
 
-if [ -x utils.sh ]; then
-	. utils.sh
-else
-	BOLD="\e[1m"
-	RED="\e[31m"
-	RESET="\e[0m"
-	echo echo -e "${RESET}$(date +%H:%m:%S) ${RED}${BOLD}'utils.sh' does not exist or is not executable!${RESET}" >&2
-	exit 1
-fi
-echo -e "${RESET}${GREEN}${BOLD}NexToo Environment Setup Script${RESET} ${BOLD}version <TAG ME>${RESET}"
-DEBUG=true
-debug 'This is a debug message'
-DEBUG=false
-error 'This is an error message'
+# The packages directory
+#PKGSDIR=/home/bjmn55/temp
+PKGSDIR=/usr/portage/packages
+# The packages file name, default: Packages
+PKGSFILE=Packages
 
-MAKE_CONF="/etc/portage/make.conf"
+# Some styles to work with
+BOLD="\e[1m"
+RED="\e[31m"
+GREEN="\e[32m"
+RESET="\e[0m"
+YELLOW="\e[33m"
 
-if [[ ! -f "${MAKE_CONF}" ]]; then
-	MAKE_CONF="/etc/make.conf"
-	if [[ ! -f "${MAKE_CONF}" ]]; then
-		# Is this even a gentoo install?
-		error "Unable to locate your systems make.conf!"
-		exit 1
+# Some global variables to help with our endeavor
+PKG="" # Package name
+MD5="" # Package MD5 sum
+SHA1="" # Package SHA1 sum
+SIZE="" # Package size
+
+# Work begins
+set -e
+
+# Fix bashs internal field seperator to use newlines instead of spaces
+IFS=$'\n';
+
+getstr() {
+	echo $(echo $1 | awk '{print $2}')
+}
+
+verify() {
+	# If the package name is blank, we can't do much
+	if [[ $PKG == "" ]]; then
+		return 0
 	fi
+
+	# Get actual package size
+	## BUG ALERT: we are always assuming the package ends with ".tbz2"
+	local P=$PKGSDIR/$PKG.tbz2
+	
+	# If the file is missing, we can't do any further testing
+	if [[ ! -e $P ]]; then
+		echo -e "${RESET}${BOLD}${PKG} ${RED}File missing${RESET}"
+		return 0
+	fi
+
+	# Some local variables to store test results and formatted strings
+	local AMD5=$(md5sum $P | awk '{print $1}')
+	local ASHA1=$(sha1sum $P | awk '{print $1}')
+	local AS=$(stat -c%s $P)
+	local MD=""
+	local SH=""
+	local SZ=""
+
+	# Compare MD5
+	if [[ $MD5 == "" || $AMD5 == "" ]]; then
+		MD="${YELLOW}"
+	else
+		[[ $AMD5 == $MD5 ]] && MD="${GREEN}" || MD="${RED}"
+	fi
+	# Format for echo
+	MD="${MD}MD5"
+
+	# Compare SHA1
+	if [[ $SHA1 == "" || $ASHA1 == "" ]]; then
+		SH="${YELLOW}"
+	else
+		[[ $ASHA1 == $SHA1 ]] && SH="${GREEN}" || SH="${RED}"
+	fi
+	# Format for echo
+	SH="${SH}SHA1"
+
+	# Compare size
+	if [[ $SIZE == "" || $AS == "" ]]; then
+		SIZE="${YELLOW}"
+	else
+		[[ $AS == $SIZE ]] && SZ="${GREEN}" || SZ="${RED}"
+	fi
+	# Format for echo
+	SZ="${SZ}SIZE"
+
+	# Echo results
+	echo -e "${RESET}${BOLD}${PKG} ${SZ} ${MD} ${SH}${RESET}"
+}
+
+while read line; do
+	# If this line starts with 'CPV', it's the package name so record it
+	if [[ $line == CPV* ]]; then
+		PKG=$(getstr $line)
+	# If this line starts with 'SIZE', record this as the file size
+	elif [[ $line == SIZE* ]]; then
+		SIZE=$(getstr $line)
+	# If this line start with 'MD5', record this as the file MD5 sum
+	elif [[ $line == MD5* ]]; then
+		MD5=$(getstr $line)
+	# If this file contains 'SHA1', record this as the file SHA1 sum
+	elif [[ $line == SHA1* ]]; then
+		SHA1=$(getstr $line)
+	# If this line is blank and the package name is not blank, we have most likely collected all the info we need to verify the file
+	elif [[ $line == "" && $PKG != "" ]]; then
+		verify
+		# After file verfication, reset the variables
+		PKG=""
+		MD5=""
+		SHA1=""
+		SIZE=""
+	fi
+done < "$PKGSDIR/$PKGSFILE"
+
+# Just in case the loop doesn't catch the last package...
+if [[ $line == "" && $PKG != "" ]]; then
+	verify
 fi
 
-status "make.conf located at ${MAKE_CONF}"
+# Return bashs internal field seperator to normal
+IFS=$' '
 
-status "Updating make.conf"
-egrep '^source /var/lib/layman/make.conf$' "${MAKE_CONF}" >/dev/null || echo 'source /var/lib/layman/make.conf' #>> "${MAKE_CONF}"
-status "make.conf updated"
+set +e
