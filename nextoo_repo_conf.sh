f518946b07cb96b0b4dbeb6cacfe672545f67f71
Benjamin Mann <bjmn55@gmail.com>
2014-01-13 04:09:56 -0800

    Added features to 'verify_binaries.sh' and created a repo config script

---
+++ nextoo_repo_conf.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+REPOS_CONF_PATH=/etc/portage
+REPOS_CONF=$REPOS_CONF_PATH/repos.conf
+# This will get a directory 'portage' added to it under which the nextoo repo will go
+NEXTOO_PATH=/usr/nextoo
+# turn on noisy debug
+DEBUG=1
+
+# are we running as root?
+if [[ $EUID -ne 0 ]]; then
+	echo "You must be root to run this script"
+	exit 1
+fi
+
+# Store the gentoo repos config info
+read -d '' GENTOO_CONF << EOL
+[DEFAULT]
+main-repo = gentoo
+
+[gentoo]
+location = /usr/portage
+sync-type = rsync
+sync-uri = rsync://gentoo.closest.myvwan.com/gentoo-portage
+EOL
+
+# Store the nextoo repos config info
+read -d '' NEXTOO_CONF << EOL
+[nextoo]
+location = $NEXTOO_PATH/portage
+sync-type = git
+sync-uri = https://github.com/Nextoo/portage-overlay.git
+EOL
+
+ERR=""
+
+echo "Adding configuration for Nextoo repo..."
+if [[ -e $REPOS_CONF ]]; then
+	# repos.conf exists
+	if [[ -f $REPOS_CONF ]]; then
+		# it's a file
+		NTC=$(grep -ic nextoo $REPOS_CONF)
+		if [[ $NTC -eq '0' ]]; then
+			# but doesn't have a listing for the nextoo repo, so add it
+			if [[ -w $REPOS_CONF ]]; then
+
+				# The line break is here in purpose...
+				echo "
+${NEXTOO_CONF}" >> $REPOS_CONF
+			else
+				# but it's not writable
+				ERR="repos.conf is not writable. Please run this script as root."
+			fi
+		fi
+	elif [[ -d $REPOS_CONF ]]; then
+		# it's a directory
+		if [[ ! -e $REPOS_CONF/nextoo.conf ]]; then
+			# it doesn't have a config for the nextoo repo, so create it
+			if [[ -w $REPOS_CONF ]]; then
+			# it's writable
+				echo "${NEXTOO_CONF}" > $REPOS_CONF/nextoo.conf
+			else
+				# it's not writable
+				ERR="repos.conf is not writable. Please run this script as root."
+			fi
+		fi
+	else
+		# it's not a file or a directory, but it exists...
+		ERR="repos.conf exists but is not a file or a directory. Aborting."
+	fi
+else
+	# it doesn't exist
+	if [[ -w $REPOS_CONF_PATH ]]; then
+		# but the parent directory is writable
+		# so create a repos directory
+		mkdir -p $REPOS_CONF
+		if [[ $? -eq '0' ]]; then
+			# and write the gentoo config
+			echo "${GENTOO_CONF}" > $REPOS_CONF/gentoo.conf
+			# and write the nextoo config
+			echo "${NEXTOO_CONF}" > $REPOS_CONF/nextoo.conf
+		else
+			ERR="Error creating directory 'repos.conf'."
+		fi
+	else
+		ERR="repos.conf does not exist and the path '${REPOS_CONF_PATH}' is not writable. Please run this script as root."
+	fi
+fi
+
+if [[ $ERR != "" ]]; then
+	echo $ERR
+	exit 1
+fi
+
+echo "Nextoo repo config in place, proceeding with installation..."
+
+# we need to have dev-vcs/git installed to clone and update the nextoo repo
+echo "Installing git, if it's not already installed..."
+emerge -1Nu dev-vcs/git
+
+if [[ ! -e $NEXTOO_PATH ]]; then
+	# make the nextoo folder
+	echo "Creating directory for Nextoo repo"
+	mkdir -p $NEXTOO_PATH
+	if [[ $? -ne '0' ]]; then
+		echo "Error creating Nextoo repo directory."
+		exit 1
+	fi
+	# change to that directory
+	echo "Changing to Nextoo repo directory"
+	cd $NEXTOO_PATH
+	# clone the repository in to that directory
+	echo "Cloning Nextoo repo..."
+	git clone https://github.com/Nextoo/portage-overlay.git portage
+	echo "All done, enjoy :D"
+else
+	echo "${NEXTOO_PATH} already exists. Skipping installation."
+fi
--- verify_binaries.sh
+++ verify_binaries.sh
@@ -3,7 +3,7 @@
 # The packages directory
 PKGSDIR=/usr/portage/packages
 # The packages file name, default: Packages
-PKGSFILE=Packages
+PKGSFILE=../Packages
 
 # Some styles to work with
 BOLD="\e[1m"
@@ -17,6 +17,10 @@ PKG="" # Package name
 MD5="" # Package MD5 sum
 SHA1="" # Package SHA1 sum
 SIZE="" # Package size
+
+CHECK_MD5=0
+CHECK_SHA1=0
+CHECK_SIZE=0
 
 # Work begins
 set -e
@@ -45,59 +49,129 @@ verify() {
 	fi
 
 	# Some local variables to store test results and formatted strings
-	local AMD5=$(md5sum $P | awk '{print $1}')
-	local ASHA1=$(sha1sum $P | awk '{print $1}')
-	local AS=$(stat -c%s $P)
+	local AS=""
+	local AMD5=""
+	local ASHA1=""
 	local MD=""
 	local SH=""
 	local SZ=""
+	local RESULT="${BOLD}${PKG}"
 
-	# Make sure we have both the recorded MD5 and the files actual MD5
-	if [[ $MD5 == "" || $AMD5 == "" ]]; then
-		MD="${YELLOW}"
-	else
-		# Compare MD5
-		[[ $AMD5 == $MD5 ]] && MD="${GREEN}" || MD="${RED}"
+	if [[ "${CHECK_SIZE}" -eq 1 ]]; then
+		AS=$(stat -c%s $P)
+		# Make sure we have both the recorded size and the files actual size
+		if [[ $SIZE == "" || $AS == "" ]]; then
+			SZ="${YELLOW}"
+		else
+			# Compare size
+			[[ $AS == $SIZE ]] && SZ="${GREEN}" || SZ="${RED}"
+		fi
+		# Format for echo
+		RESULT="${RESULT} ${SZ}SIZE"
 	fi
-	# Format for echo
-	MD="${MD}MD5"
 
-	# Make sure we have both the recorded SHA1 and the files actual SHA1
-	if [[ $SHA1 == "" || $ASHA1 == "" ]]; then
-		SH="${YELLOW}"
-	else
-		# Compare SHA1
-		[[ $ASHA1 == $SHA1 ]] && SH="${GREEN}" || SH="${RED}"
+	if [[ "${CHECK_MD5}" -eq 1 ]]; then
+		AMD5=$(md5sum $P | awk '{print $1}')
+		# Make sure we have both the recorded MD5 and the files actual MD5
+		if [[ $MD5 == "" || $AMD5 == "" ]]; then
+			MD="${YELLOW}"
+		else
+			# Compare MD5
+			[[ $AMD5 == $MD5 ]] && MD="${GREEN}" || MD="${RED}"
+		fi
+		# Format for echo
+		RESULT="${RESULT} ${MD}MD5"
 	fi
-	# Format for echo
-	SH="${SH}SHA1"
 
-	# Make sure we have both the recorded size and the files actual size
-	if [[ $SIZE == "" || $AS == "" ]]; then
-		SZ="${YELLOW}"
-	else
-		# Compare size
-		[[ $AS == $SIZE ]] && SZ="${GREEN}" || SZ="${RED}"
+	if [[ "${CHECK_SHA1}" -eq 1 ]]; then
+		ASHA1=$(sha1sum $P | awk '{print $1}')
+		# Make sure we have both the recorded SHA1 and the files actual SHA1
+		if [[ $SHA1 == "" || $ASHA1 == "" ]]; then
+			SH="${YELLOW}"
+		else
+			# Compare SHA1
+			[[ $ASHA1 == $SHA1 ]] && SH="${GREEN}" || SH="${RED}"
+		fi
+		# Format for echo
+		RESULT="${RESULT} ${SH}SHA1"
 	fi
-	# Format for echo
-	SZ="${SZ}SIZE"
 
 	# Echo results
-	echo -e "${RESET}${BOLD}${PKG} ${SZ} ${MD} ${SH}${RESET}"
+	echo -e "${RESET}${RESULT}${RESET}"
 }
+
+function usage() {
+	echo -e "${RESET}${GREEN}${BOLD}Nextoo Verify Binaries Script${RESET} ${BOLD}version <TAG ME>${RESET}"
+	cat <<-EOU
+		Usage:	$(basename "${0}") [option(s)] [binary packages path]
+
+		Options:
+		    --md5		Run MD5 tests
+		    --sha1		Run SHA1 test
+		    --size		Make sure the binary size is correct
+		    -a, --all		Run all tests
+		    -h, --help		Show this message and exit
+		
+		If the binary packages path is not set, /usr/portage/packages is used.
+
+	EOU
+}
+# eval set -- "${args}"
+while true; do
+	case "$1" in
+		--md5)
+			CHECK_MD5=1
+			shift
+			;;
+
+		--sha1)
+			CHECK_SHA1=1
+			shift
+			;;
+
+		--size)
+			CHECK_SIZE=1
+			shift
+			;;
+
+		-a | --all)
+			CHECK_MD5=1
+			CHECK_SHA1=1
+			CHECK_SIZE=1
+			shift
+			;;
+
+		-h | --help)
+			usage
+			exit 0
+			;;
+
+		*)
+			break
+			;;
+	esac
+done
+
+if [[ "${CHECK_MD5}" -ne 1 && "${CHECK_SHA1}" -ne 1 && "${CHECK_SIZE}" -ne 1 ]]; then
+	usage
+	exit 1
+fi
+if [[ ${1} != "" ]]; then
+	PKGSDIR="${1}"
+fi
 
 while read line; do
 	# If this line starts with 'CPV', it's the package name so record it
 	if [[ $line == CPV* ]]; then
 		PKG=$(getstr $line)
 	# If this line starts with 'SIZE', record this as the file size
-	elif [[ $line == SIZE* ]]; then
+	elif [[ $CHECK_SIZE && $line == SIZE* ]]; then
 		SIZE=$(getstr $line)
 	# If this line start with 'MD5', record this as the file MD5 sum
-	elif [[ $line == MD5* ]]; then
+	elif [[ $CHECK_MD5 && $line == MD5* ]]; then
 		MD5=$(getstr $line)
 	# If this file contains 'SHA1', record this as the file SHA1 sum
-	elif [[ $line == SHA1* ]]; then
+	elif [[ $CHECK_SHA1 && $line == SHA1* ]]; then
 		SHA1=$(getstr $line)
 	# If this line is blank and the package name is not blank, we have most likely collected all the info we need to verify the file
 	elif [[ $line == "" && $PKG != "" ]]; then
