8856a08c4cb73f27e3141b19b1a36f84d1290168
Aaron Ten Clay <aarontc@aarontc.com>
2014-01-13 21:58:59 -0800

    Update nextoo_repo_conf.sh
    Streamlined script to do less manual error checking after adding `set -e`, and handle all known possibilities for portage repos.conf manifestations

--- nextoo_repo_conf.sh
+++ nextoo_repo_conf.sh
@@ -1,115 +1,58 @@
 #!/bin/bash
-REPOS_CONF_PATH=/etc/portage
-REPOS_CONF=$REPOS_CONF_PATH/repos.conf
-# This will get a directory 'portage' added to it under which the nextoo repo will go
+REPOS_CONF=/etc/portage/repos.conf
+# This will get a directory 'portage' added to it under which the Nextoo repo will go
 NEXTOO_PATH=/usr/nextoo
-# turn on noisy debug
-DEBUG=1
+NEXTOO_PORTAGE_URI=https://github.com/Nextoo/portage-overlay.git
+
+# Exit immediately on any unhandled error
+set -e
 
 # are we running as root?
-if [[ $EUID -ne 0 ]]; then
+if [[ "${EUID}" -ne '0' ]]; then
 	echo "You must be root to run this script" >&2
 	exit 1
 fi
 
-# Store the gentoo repos config info
-read -d '' GENTOO_CONF << EOL
-[DEFAULT]
-main-repo = gentoo
-
-[gentoo]
-location = /usr/portage
-sync-type = rsync
-sync-uri = rsync://gentoo.closest.myvwan.com/gentoo-portage
-EOL
-
-# Store the nextoo repos config info
+# Store the Nextoo repo config info
 read -d '' NEXTOO_CONF << EOL
 [nextoo]
-location = $NEXTOO_PATH/portage
-sync-type = git
-sync-uri = https://github.com/Nextoo/portage-overlay.git
+	location = $NEXTOO_PATH/portage
+	sync-type = git
+	sync-uri = ${NEXTOO_PORTAGE_URI}
 EOL
 
-ERR=""
-
-echo "Adding configuration for Nextoo repo..."
-if [[ -e $REPOS_CONF ]]; then
-	# repos.conf exists
-	if [[ -f $REPOS_CONF ]]; then
-		# it's a file
-		NTC=$(grep -ic nextoo $REPOS_CONF)
-		if [[ $NTC -eq '0' ]]; then
-			# but doesn't have a listing for the nextoo repo, so add it
-			if [[ -w $REPOS_CONF ]]; then
-
-				echo -e "\n${NEXTOO_CONF}" >> $REPOS_CONF
-			else
-				# but it's not writable
-				ERR="repos.conf is not writable. Please run this script as root."
-			fi
-		fi
-	elif [[ -d $REPOS_CONF ]]; then
-		# it's a directory
-		if [[ ! -e $REPOS_CONF/nextoo.conf ]]; then
-			# it doesn't have a config for the nextoo repo, so create it
-			if [[ -w $REPOS_CONF ]]; then
-			# it's writable
-				echo "${NEXTOO_CONF}" > $REPOS_CONF/nextoo.conf
-			else
-				# it's not writable
-				ERR="repos.conf is not writable. Please run this script as root."
-			fi
-		fi
-	else
-		# it's not a file or a directory, but it exists...
-		ERR="repos.conf exists but is not a file or a directory. Aborting."
+echo "Adding configuration for Nextoo portage repository..."
+if [[ -d "${REPOS_CONF}" ]]; then
+	# REPOS_CONF is a directory, so check for a file containing the Nextoo repo
+	if ! egrep '^\[nextoo\]$' "${REPOS_CONF}"/* >/dev/null; then
+		# Don't clobber the data if it exists...
+		set -o noclobber
+		echo -e "\n${NEXTOO_CONF}" > "${REPOS_CONF}"/nextoo.conf
+	fi
+elif [[ -f "${REPOS_CONF}" ]]; then
+	# REPOS_CONF is a file, so check for the Nextoo repo in the file
+	if ! egrep '^\[nextoo\]$' "${REPOS_CONF}" >/dev/null; then
+		# Append the Nextoo configuration
+		echo -e "\n\n${NEXTOO_CONF}" >> "${REPOS_CONF}"
 	fi
 else
-	# it doesn't exist
-	if [[ -w $REPOS_CONF_PATH ]]; then
-		# but the parent directory is writable
-		# so create a repos directory
-		mkdir -p $REPOS_CONF
-		if [[ $? -eq '0' ]]; then
-			# and write the gentoo config
-			echo "${GENTOO_CONF}" > $REPOS_CONF/gentoo.conf
-			# and write the nextoo config
-			echo "${NEXTOO_CONF}" > $REPOS_CONF/nextoo.conf
-		else
-			ERR="Error creating directory 'repos.conf'."
-		fi
-	else
-		ERR="repos.conf does not exist and the path '${REPOS_CONF_PATH}' is not writable. Please run this script as root."
-	fi
+	# No file or directory for repos.conf so create it (or maybe it was something silly like a device node)
+	mkdir -p "${REPOS_CONF}"
+	set -o noclobber
+	echo "${NEXTOO_CONF}" > "${REPOS_CONF}"/nextoo.conf
 fi
 
-if [[ "${ERR}" != "" ]]; then
-	echo $ERR >&2
-	exit 1
-fi
+echo "Nextoo portage repository config in place, proceeding with installation..."
 
-echo "Nextoo repo config in place, proceeding with installation..."
+# we need to have dev-vcs/git installed to clone and update the Nextoo repo
+echo "Merging git..."
+emerge --newuse --update dev-vcs/git
 
-# we need to have dev-vcs/git installed to clone and update the nextoo repo
-echo "Installing git, if it's not already installed..."
-emerge -1Nu dev-vcs/git
+# Create the Nextoo base directory if it doesn't exist
+[[ ! -d "${NEXTOO_PATH}" ]] && mkdir -p "${NEXTOO_PATH}"
 
-if [[ ! -e $NEXTOO_PATH ]]; then
-	# make the nextoo folder
-	echo "Creating directory for Nextoo repo"
-	mkdir -p $NEXTOO_PATH
-	if [[ $? -ne '0' ]]; then
-		echo "Error creating Nextoo repo directory." >&2
-		exit 1
-	fi
-	# change to that directory
-	echo "Changing to Nextoo repo directory"
-	cd $NEXTOO_PATH
-	# clone the repository in to that directory
-	echo "Cloning Nextoo repo..."
-	git clone https://github.com/Nextoo/portage-overlay.git portage
-	echo "All done, enjoy :D"
-else
-	echo "${NEXTOO_PATH} already exists. Skipping installation."
+# Clone the Nextoo portage repository the first time if needed
+if [[ ! -d "${NEXTOO_PATH}"/portage ]]; then
+	cd "${NEXTOO_PATH}"
+	git clone "${NEXTOO_PORTAGE_URI}" portage
 fi
