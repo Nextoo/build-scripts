1721a2086da60c2afa0ae2b31ce1583218a4090b
Chris <chris.haverkate@gmail.com>
2014-01-15 21:38:27 -0800

    Adding optional printing of date stamp

--- utils.sh
+++ utils.sh
@@ -9,9 +9,15 @@ YELLOW="\e[33m"
 
 set -e
 
+function getDate() {
+	if [[ "${PRINT_DATE}" == 'true' ]]; then
+		echo "$(date +%Y-%m-%d\ %H:%M:%S)"
+	fi
+}
+
 function debug() {
 	if [[ "${DEBUG}" == 'true' ]]; then
-		echo -e "${RESET}$(date +%Y-%m-%d\ %H:%M:%S) ${BOLD}${YELLOW}${*}${RESET}"
+		echo -e "${RESET}$(getDate) ${BOLD}${YELLOW}${*}${RESET}"
 	fi
 }
 
@@ -27,7 +33,7 @@ function finish() {
 }
 
 function error() {
-	echo -e "${RESET}$(date +%Y-%m-%d\ %H:%M:%S) ${RED}${BOLD}${*}${RESET}" >&2
+	echo -e "${RESET}$(getDate) ${RED}${BOLD}${*}${RESET}" >&2
 }
 
 function run() {
@@ -44,7 +50,7 @@ function run() {
 }
 
 function status() {
-	echo -e "${RESET}$(date +%Y-%m-%d\ %H:%M:%S) ${BOLD}${*}${RESET}"
+	echo -e "${RESET}$(getDate) ${BOLD}${*}${RESET}"
 }
 
 function ensure_root() {
